90a7b57d4776f3319e22fa9ef11e76a9227baaef
udara-pk <53249224+udara-pk@users.noreply.github.com>
2025-09-03 10:39:25 +0530

    Many to Many Relationship

--- media/src/main/java/com/social/media/model/SocialUser.java
+++ media/src/main/java/com/social/media/model/SocialUser.java
@@ -3,7 +3,9 @@ package com.social.media.model;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class SocialUser {
@@ -16,4 +18,12 @@ public class SocialUser {
 
     @OneToMany(mappedBy = "socialUser")
     private List<Post> posts = new ArrayList<Post>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_group",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    private Set<SocialGroup> groups = new HashSet<>();
 }
